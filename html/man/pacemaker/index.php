5490b7aa2d3d1582557c6241e319fd92eb101634
Andrew Beekhof <andrew@beekhof.net>
2012-11-21 08:29:00 +1100

    man page index formatting

--- html/man/pacemaker/index.php
+++ html/man/pacemaker/index.php
@@ -13,7 +13,7 @@
 
      <section id="main">
      <h1>Pacemaker Command Line Tools</h1>
-     <h2>Summary</h2>
+     <h2>Tool Summary</h2>
 
 <?php
      function get_desc($man) 
@@ -80,19 +80,19 @@ foreach ($mans as $m) {
 	   <span class="term">Monitoring the Cluster's Status</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">crm_mon</strong> command
+	   <p>The <strong class="inline-command">crm_mon</strong> command
 	      allows you to monitor your cluster's status and
 	      configuration. Its output includes the number of nodes,
 	      uname, uuid, status, the resources configured in your
 	      cluster, and the current status of each. The output of
 	     
-	     <strong class="command">crm_mon</strong> 
+	     <strong class="inline-command">crm_mon</strong> 
 
 	     can be displayed at the console or printed into an HTML
 	     file. When provided with a cluster configuration file
 	     without the status section,
 
-	     <strong class="command">crm_mon</strong>
+	     <strong class="inline-command">crm_mon</strong>
 	     
 	     creates an overview of nodes and resources as specified
 	     in the file. See <a href="crm_mon.8.html"
@@ -105,7 +105,7 @@ foreach ($mans as $m) {
 	   <span class="term">Managing the CIB</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">cibadmin</strong> command is
+	   <p>The <strong class="inline-command">cibadmin</strong> command is
 	      the low-level administrative command for manipulating
 	      the Pacemaker CIB. It can be used to dump all or part of
 	      the CIB, update all or part of it, modify all or part of
@@ -121,7 +121,7 @@ foreach ($mans as $m) {
 	   <span class="term">Managing Configuration Changes</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">crm_diff</strong> command
+	   <p>The <strong class="inline-command">crm_diff</strong> command
 	      assists you in creating and applying XML patches.  This
 	      can be useful for visualizing the changes between two
 	      versions of the cluster configuration or saving changes
@@ -138,7 +138,7 @@ foreach ($mans as $m) {
 	   <span class="term">Manipulating CIB Attributes</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">crm_attribute</strong>
+	   <p>The <strong class="inline-command">crm_attribute</strong>
 	      command lets you query and manipulate node attributes
 	      and cluster configuration options that are used in the
 	      CIB. See <a href="crm_attribute.8.html"
@@ -150,20 +150,20 @@ foreach ($mans as $m) {
 	   <span class="term">Validating the Cluster Configuration</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">crm_verify</strong> command
+	   <p>The <strong class="inline-command">crm_verify</strong> command
 	      checks the configuration database (CIB) for consistency
 	      and other problems. It can check a file containing the
 	      configuration or connect to a running cluster. It
 	      reports two classes of problems. Errors must be fixed
 	      before Pacemaker can work properly while warning
 	      resolution is up to the administrator.
-	       <strong class="command">crm_verify</strong> assists in
+	       <strong class="inline-command">crm_verify</strong> assists in
 	      creating new or modified configurations. You can take a
 	      local copy of a CIB in the running cluster, edit it,
 	      validate it
-	      using <strong class="command">crm_verify</strong> , then
+	      using <strong class="inline-command">crm_verify</strong> , then
 	      put the new configuration into effect using
-	       <strong class="command">cibadmin</strong>
+	       <strong class="inline-command">cibadmin</strong>
 	     . See <a href="crm_verify.8.html" title="crm_verify">
 	       <span class="refentrytitle">crm_verify</span>(8)</a>
 	     for a detailed introduction to this tool's usage and
@@ -174,7 +174,7 @@ foreach ($mans as $m) {
 	   <span class="term">Managing Resource Configurations</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">crm_resource</strong>
+	   <p>The <strong class="inline-command">crm_resource</strong>
 	      command performs various resource-related actions on the
 	      cluster. It lets you modify the definition of configured
 	      resources, start and stop resources, or delete and
@@ -189,7 +189,7 @@ foreach ($mans as $m) {
 	   <span class="term">Managing Resource Fail Counts</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">crm_failcount</strong>
+	   <p>The <strong class="inline-command">crm_failcount</strong>
 	      command queries the number of failures per resource on a
 	      given node. This tool can also be used to reset the
 	      failcount, allowing the resource to again run on nodes
@@ -206,11 +206,11 @@ foreach ($mans as $m) {
 	 <dd>
 	   <p>UUIDs are used to identify cluster nodes to ensure that
 	     they can always be uniquely identified. The command
-	       <strong class="command">crm_uuid</strong> displays the
+	       <strong class="inline-command">crm_uuid</strong> displays the
 	     UUID of the node on which it is run. In very rare
 	     circumstances, it may be necessary to set a node's UUID
 	     to a known value.  This can also be achieved with
-	       <strong class="command">crm_uuid</strong> , but you
+	       <strong class="inline-command">crm_uuid</strong> , but you
 	     should use this command with extreme caution. For more
 	     information, refer to <a href="crm_uuid.8.html"
 	     title="crm_uuid">
@@ -220,7 +220,7 @@ foreach ($mans as $m) {
 	   <span class="term">Managing a Node's Standby Status</span>
 	 </dt>
 	 <dd>
-	   <p>The <strong class="command">crm_standby</strong> command
+	   <p>The <strong class="inline-command">crm_standby</strong> command
 	     can manipulate a node's standby attribute. Any node in
 	     standby mode is no longer eligible to host resources and
 	     any resources that are there must be moved.  Standby mode
